cfe31a3ac424b199227bf4ee67a676d2c51b4b44
Jean-Christophe BEGUE <jc.begue@pm.me>
2020-08-12 09:26:06 +0200

    Data frame streaming with poll_data()

--- h3/src/frame/mod.rs
+++ h3/src/frame/mod.rs
@@ -1,15 +1,16 @@
 mod buf;
 
 use std::{
-    io,
+    cmp,
+    io::{self, ErrorKind},
     task::{Context, Poll},
 };
 
-use bytes::{Buf, Bytes};
+use bytes::{Buf, Bytes, BytesMut};
 
 use crate::{
     proto::{
-        frame::{self, Frame, FrameHeader, PartialData},
+        frame::{self, Frame},
         ErrorCode,
     },
     quic::RecvStream,
@@ -20,38 +21,105 @@ pub struct FrameStream<R: RecvStream> {
     recv: R,
     bufs: BufList<Bytes>,
     decoder: FrameDecoder,
+    remaining_data: u64,
 }
 
 impl<R> FrameStream<R>
 where
     R: RecvStream<Buf = Bytes>,
 {
+    pub fn new(recv: R) -> Self {
+        Self {
+            recv,
+            bufs: BufList::new(),
+            decoder: FrameDecoder::default(),
+            remaining_data: 0,
+        }
+    }
+
     pub fn poll_next(&mut self, cx: &mut Context<'_>) -> Poll<Result<Option<Frame>, Error>> {
-        let end = match self.recv.poll_data(cx) {
-            Poll::Ready(Err(e)) => return Poll::Ready(Err(e.into().into())),
-            Poll::Pending => false,
-            Poll::Ready(Ok(None)) => true,
-            Poll::Ready(Ok(Some(d))) => {
-                self.bufs.push(d);
-                false
-            }
+        assert!(
+            self.remaining_data == 0,
+            "There is still data to read, please call poll_data() until it returns None."
+        );
+
+        loop {
+            let end = self.try_recv(cx)?;
+
+            return match self.decoder.decode(&mut self.bufs)? {
+                Some(Frame::Data { len }) => {
+                    self.remaining_data = len;
+                    Poll::Ready(Ok(Some(Frame::Data { len })))
+                }
+                Some(frame) => Poll::Ready(Ok(Some(frame))),
+                None => match end {
+                    // Recieved a chunk but frame is incomplete, poll until we get `Pending`.
+                    Poll::Ready(false) => continue,
+                    Poll::Pending => Poll::Pending,
+                    Poll::Ready(true) => {
+                        if self.bufs.has_remaining() {
+                            // Reached the end of recieve stream, but there is still some data:
+                            // The frame is incomplete.
+                            Poll::Ready(Err(Error::Io(ErrorKind::UnexpectedEof.into())))
+                        } else {
+                            Poll::Ready(Ok(None))
+                        }
+                    }
+                },
+            };
+        }
+    }
+
+    pub fn poll_data(&mut self, cx: &mut Context<'_>) -> Poll<Result<Option<Bytes>, Error>> {
+        if self.remaining_data == 0 {
+            return Poll::Ready(Ok(None));
         };
 
-        match self.decoder.decode(&mut self.bufs)? {
-            Some(frame) => Poll::Ready(Ok(Some(frame))),
-            None if end => Poll::Ready(Ok(None)),
-            None => Poll::Pending,
+        let end = match self.try_recv(cx)? {
+            Poll::Pending => return Poll::Pending,
+            Poll::Ready(end) => end,
+        };
+
+        let read_size = cmp::min(self.remaining_data as usize, self.bufs.remaining());
+        let mut data = BytesMut::with_capacity(read_size);
+        while data.len() < read_size {
+            let chunk = self.bufs.bytes();
+            let chunk_len = cmp::min(read_size, chunk.remaining());
+            data.extend_from_slice(&chunk[..chunk_len]);
+            self.bufs.advance(chunk_len);
         }
+
+        match (data.len(), end) {
+            (0, true) => return Poll::Ready(Ok(None)),
+            (0, false) => return Poll::Pending,
+            (x, true) if (x as u64) < self.remaining_data => {
+                return Poll::Ready(Err(Error::Io(ErrorKind::UnexpectedEof.into())));
+            }
+            (x, _) => self.remaining_data -= x as u64,
+        };
+
+        Poll::Ready(Ok(Some(Bytes::from(data))))
     }
 
     pub fn reset(&mut self, error_code: ErrorCode) {
         let _ = self.recv.stop_sending(error_code.0.into());
     }
+
+    fn try_recv(&mut self, cx: &mut Context<'_>) -> Poll<Result<bool, Error>> {
+        match self.recv.poll_data(cx) {
+            Poll::Ready(Err(e)) => return Poll::Ready(Err(e.into().into())),
+            Poll::Pending => Poll::Pending,
+            Poll::Ready(Ok(None)) => Poll::Ready(Ok(true)),
+            Poll::Ready(Ok(Some(d))) => {
+                self.bufs.push(d);
+                Poll::Ready(Ok(false))
+            }
+        }
+    }
 }
 
 #[derive(Default)]
 pub struct FrameDecoder {
-    partial: Option<PartialData>,
     expected: Option<usize>,
 }
 
@@ -69,17 +137,6 @@ impl FrameDecoder {
             return Ok(None);
         }
 
-        if let Some(ref mut partial) = self.partial {
-            let (pos, frame) = decode!(src, |cur| Frame::Data(partial.decode_data(cur)));
-            src.advance(pos);
-
-            if partial.remaining() == 0 {
-                self.partial = None;
-            }
-
-            return Ok(Some(frame));
-        }
-
         if let Some(min) = self.expected {
             if src.remaining() < min {
                 return Ok(None);
@@ -89,18 +146,6 @@ impl FrameDecoder {
         let (pos, decoded) = decode!(src, |cur| Frame::decode(cur));
 
         match decoded {
-            Err(frame::Error::IncompleteData) => {
-                let (pos, decoded) = decode!(src, |cur| PartialData::decode(cur));
-                let (partial, frame) = decoded?;
-                src.advance(pos);
-                self.expected = None;
-                self.partial = Some(partial);
-                if frame.len() > 0 {
-                    Ok(Some(Frame::Data(frame)))
-                } else {
-                    Ok(None)
-                }
-            }
             Err(frame::Error::UnknownFrame(_)) => {
                 src.advance(pos);
                 self.expected = None;
@@ -153,12 +198,19 @@ impl From<io::Error> for Error {
 mod tests {
     use super::*;
     use crate::proto::frame;
+
+    use std::collections::VecDeque;
+
     use assert_matches::assert_matches;
-    use bytes::BytesMut;
+    use bytes::BufMut;
+    use futures::future::poll_fn;
+    use tokio;
+
+    // Decoder
 
     #[test]
     fn one_frame() {
-        let frame = frame::HeadersFrame {
+        let frame = frame::Headers {
             encoded: b"salut"[..].into(),
         };
 
@@ -172,7 +224,7 @@ mod tests {
 
     #[test]
     fn incomplete_frame() {
-        let frame = frame::HeadersFrame {
+        let frame = frame::Headers {
             encoded: b"salut"[..].into(),
         };
 
@@ -187,7 +239,7 @@ mod tests {
 
     #[test]
     fn header_spread_multiple_buf() {
-        let frame = frame::HeadersFrame {
+        let frame = frame::Headers {
             encoded: b"salut"[..].into(),
         };
 
@@ -205,7 +257,7 @@ mod tests {
     #[test]
     fn varint_spread_multiple_buf() {
         let payload = "salut".repeat(1024);
-        let frame = frame::HeadersFrame {
+        let frame = frame::Headers {
             encoded: payload.into(),
         };
 
@@ -222,28 +274,191 @@ mod tests {
 
     #[test]
     fn two_frames_then_incomplete() {
-        let frames = [
-            Frame::Headers(frame::HeadersFrame {
-                encoded: b"header"[..].into(),
-            }),
-            Frame::Data(frame::DataFrame {
-                payload: b"body"[..].into(),
-            }),
-            Frame::Headers(frame::HeadersFrame {
-                encoded: b"trailer"[..].into(),
-            }),
-        ];
-
         let mut buf = BytesMut::with_capacity(64);
-        for frame in frames.iter() {
-            frame.encode(&mut buf);
-        }
+        Frame::Headers(frame::Headers {
+            encoded: b"header"[..].into(),
+        })
+        .encode(&mut buf);
+        Frame::Data { len: 4 }.encode(&mut buf);
+        buf.put_slice(&b"body"[..]);
+        Frame::Headers(frame::Headers {
+            encoded: b"trailer"[..].into(),
+        })
+        .encode(&mut buf);
+
         buf.truncate(buf.len() - 1);
         let mut buf = BufList::from(buf);
 
         let mut decoder = FrameDecoder::default();
         assert_matches!(decoder.decode(&mut buf), Ok(Some(Frame::Headers(_))));
-        assert_matches!(decoder.decode(&mut buf), Ok(Some(Frame::Data(_))));
+        assert_matches!(decoder.decode(&mut buf), Ok(Some(Frame::Data { len: 4 })));
         assert_matches!(decoder.decode(&mut buf), Ok(None));
+    }
+
+    // FrameStream
+
+    macro_rules! assert_poll_matches {
+        ($poll_fn:expr, $match:pat) => {
+            assert_matches!(
+                poll_fn($poll_fn).await,
+                $match
+            );
+        };
+        ($poll_fn:expr, $match:pat if $cond:expr ) => {
+            assert_matches!(
+                poll_fn($poll_fn).await,
+                $match if $cond
+            );
+        }
+    }
+
+    #[tokio::test]
+    async fn poll_full_request() {
+        let mut recv = FakeRecv::default();
+        let mut buf = BytesMut::with_capacity(64);
+
+        Frame::Headers(frame::Headers {
+            encoded: b"header"[..].into(),
+        })
+        .encode(&mut buf);
+        Frame::Data { len: 4 }.encode(&mut buf);
+        buf.put_slice(&b"body"[..]);
+        Frame::Headers(frame::Headers {
+            encoded: b"trailer"[..].into(),
+        })
+        .encode(&mut buf);
+        recv.chunk(buf.freeze());
+        let mut stream = FrameStream::new(recv);
+
+        assert_poll_matches!(
+            |mut cx| stream.poll_next(&mut cx),
+            Ok(Some(Frame::Headers(_)))
+        );
+        assert_poll_matches!(
+            |mut cx| stream.poll_next(&mut cx),
+            Ok(Some(Frame::Data { len: 4 }))
+        );
+        assert_poll_matches!(
+            |mut cx| stream.poll_data(&mut cx),
+            Ok(Some(b)) if b.remaining() == 4
+        );
+        assert_poll_matches!(
+            |mut cx| stream.poll_next(&mut cx),
+            Ok(Some(Frame::Headers(_)))
+        );
+    }
+
+    #[tokio::test]
+    async fn poll_next_incomplete_frame() {
+        let mut recv = FakeRecv::default();
+        let mut buf = BytesMut::with_capacity(64);
+
+        Frame::Headers(frame::Headers {
+            encoded: b"header"[..].into(),
+        })
+        .encode(&mut buf);
+        let mut buf = buf.freeze();
+        recv.chunk(buf.split_to(buf.len() - 1));
+        let mut stream = FrameStream::new(recv);
+
+        assert_poll_matches!(
+            |mut cx| stream.poll_next(&mut cx),
+            Err(Error::Io(e)) if e.kind() == ErrorKind::UnexpectedEof
+        );
+    }
+
+    #[tokio::test]
+    #[should_panic(
+        expected = "There is still data to read, please call poll_data() until it returns None"
+    )]
+    async fn poll_next_reamining_data() {
+        let mut recv = FakeRecv::default();
+        let mut buf = BytesMut::with_capacity(64);
+
+        Frame::Data { len: 4 }.encode(&mut buf);
+        recv.chunk(buf.freeze());
+        let mut stream = FrameStream::new(recv);
+
+        assert_poll_matches!(
+            |mut cx| stream.poll_next(&mut cx),
+            Ok(Some(Frame::Data { len: 4 }))
+        );
+
+        // There is still data to consume, poll_next should panic
+        let _ = poll_fn(|mut cx| stream.poll_next(&mut cx)).await;
+    }
+
+    #[tokio::test]
+    async fn poll_data_split() {
+        let mut recv = FakeRecv::default();
+        let mut buf = BytesMut::with_capacity(64);
+
+        // Body is split into two bufs
+        Frame::Data { len: 4 }.encode(&mut buf);
+        buf.put_slice(&b"body"[..]);
+        let mut buf = buf.freeze();
+        recv.chunk(buf.split_to(buf.len() - 2));
+        recv.chunk(buf);
+        let mut stream = FrameStream::new(recv);
+
+        assert_poll_matches!(
+            |mut cx| stream.poll_next(&mut cx),
+            Ok(Some(Frame::Data { len: 4 }))
+        );
+        assert_poll_matches!(
+            |mut cx| stream.poll_data(&mut cx),
+            Ok(Some(b)) if b.remaining() == 4
+        );
+    }
+
+    #[tokio::test]
+    async fn poll_data_unexpected_end() {
+        let mut recv = FakeRecv::default();
+        let mut buf = BytesMut::with_capacity(64);
+
+        // Truncated body
+        Frame::Data { len: 4 }.encode(&mut buf);
+        buf.put_slice(&b"b"[..]);
+        recv.chunk(buf.freeze());
+        let mut stream = FrameStream::new(recv);
+
+        assert_poll_matches!(
+            |mut cx| stream.poll_next(&mut cx),
+            Ok(Some(Frame::Data { len: 4 }))
+        );
+        assert_poll_matches!(
+            |mut cx| stream.poll_data(&mut cx),
+            Err(Error::Io(e)) if e.kind() == ErrorKind::UnexpectedEof
+        );
+    }
+
+    // Helpers
+
+    #[derive(Default)]
+    struct FakeRecv {
+        chunks: VecDeque<Bytes>,
+    }
+
+    impl FakeRecv {
+        fn chunk(&mut self, buf: Bytes) -> &mut Self {
+            self.chunks.push_back(buf.into());
+            self
+        }
+    }
+
+    impl RecvStream for FakeRecv {
+        type Buf = Bytes;
+        type Error = io::Error;
+
+        fn poll_data(
+            &mut self,
+            _: &mut Context<'_>,
+        ) -> Poll<Result<Option<Self::Buf>, Self::Error>> {
+            Poll::Ready(Ok(self.chunks.pop_front()))
+        }
+
+        fn stop_sending(&mut self, _: u64) {
+            unimplemented!()
+        }
     }
 }
