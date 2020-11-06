edabefdba8704fa34d1efb2b64c55a3379989e40
Jean-Christophe BEGUE <jc.begue@pm.me>
2020-11-06 16:28:28 +0100

    Cleanup some dead code

--- h3/src/lib.rs
+++ h3/src/lib.rs
@@ -1,14 +1,10 @@
-#[allow(dead_code)]
 pub mod client;
 #[deny(missing_docs)]
 pub mod quic;
-#[allow(dead_code)]
 pub mod server;
 
-// TODO: remove once methods are effectively used through public API.
 #[allow(dead_code)]
 mod connection;
-#[allow(dead_code)]
 mod frame;
 mod proto;
 #[allow(dead_code)]
