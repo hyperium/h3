23e71f95bdf4662b9867d7e53de44724fdb3efa8
Sean McArthur <sean@seanmonstar.com>
2020-12-11 17:21:53 -0800

    Update client example to log to stderr

--- examples/client.rs
+++ examples/client.rs
@@ -5,6 +5,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tracing_subscriber::fmt()
         .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
         .with_span_events(tracing_subscriber::fmt::format::FmtSpan::FULL)
+        .with_writer(std::io::stderr)
         .init();
 
     let dest = std::env::args()
