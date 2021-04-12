eccd75c5bfeaa8a46327c696482082b53eabf071
Jean-Christophe BEGUE <jc.begue@pm.me>
2021-04-12 08:15:07 +0200

    Read a precise number of bytes in RecvStream

--- h3-quinn/src/lib.rs
+++ h3-quinn/src/lib.rs
@@ -141,6 +141,14 @@ where
         self.recv.poll_data(cx)
     }
 
+    fn poll_read(
+        &mut self,
+        buf: &mut [u8],
+        cx: &mut task::Context<'_>,
+    ) -> Poll<Result<Option<usize>, Self::Error>> {
+        self.recv.poll_read(buf, cx)
+    }
+
     fn stop_sending(&mut self, error_code: u64) {
         self.recv.stop_sending(error_code)
     }
@@ -196,6 +204,14 @@ impl quic::RecvStream for RecvStream {
             .read_chunk(usize::MAX, true)
             .poll_unpin(cx))?
         .map(|c| (c.bytes))))
+    }
+
+    fn poll_read(
+        &mut self,
+        buf: &mut [u8],
+        cx: &mut task::Context<'_>,
+    ) -> Poll<Result<Option<usize>, Self::Error>> {
+        Poll::Ready(Ok(ready!(self.stream.read(buf).poll_unpin(cx))?))
     }
 
     fn stop_sending(&mut self, error_code: u64) {
