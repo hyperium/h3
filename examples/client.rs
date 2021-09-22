6611e4e2f3cf70b39140f107cd12617d2d96630f
Jean-Christophe BEGUE <jc.begue@pm.me>
2021-09-22 17:27:18 +0200

    Allow insecure client example (#52)
    * Allow client example to connect to insecure server
    
    Add --insecure option
    
    * Update server ALPN to h3-29

--- examples/client.rs
+++ examples/client.rs
@@ -1,4 +1,22 @@
+use std::sync::{Arc, Mutex};
+
+use h3_quinn::quinn;
+use rustls;
+use structopt::StructOpt;
 use tokio::io::AsyncWriteExt;
+use webpki;
+
+static ALPN: &[u8] = b"h3-29";
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "server")]
+struct Opt {
+    #[structopt(long)]
+    pub insecure: bool,
+
+    #[structopt()]
+    pub uri: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -8,11 +26,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .with_writer(std::io::stderr)
         .init();
 
-    let dest = std::env::args()
-        .nth(1)
-        .ok_or("must include destination argument")?;
+    let opt = Opt::from_args();
 
-    let dest = dest.parse::<http::Uri>()?;
+    let dest = opt.uri.parse::<http::Uri>()?;
 
     if dest.scheme() != Some(&http::uri::Scheme::HTTPS) {
         Err("destination scheme must be 'https'")?;
@@ -35,10 +51,28 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // quinn setup
     let mut client_config = h3_quinn::quinn::ClientConfigBuilder::default();
-    client_config.protocols(&[b"h3-29"]);
+    client_config.protocols(&[ALPN]);
 
     let mut client_endpoint_builder = h3_quinn::quinn::Endpoint::builder();
-    client_endpoint_builder.default_client_config(client_config.build());
+
+    if !opt.insecure {
+        client_endpoint_builder.default_client_config(client_config.build());
+    } else {
+        let mut tls_config = rustls::ClientConfig::new();
+        tls_config.versions = vec![rustls::ProtocolVersion::TLSv1_3];
+        tls_config.enable_early_data = true;
+        tls_config
+            .dangerous()
+            .set_certificate_verifier(Arc::new(YesVerifier));
+        tls_config.alpn_protocols = vec![ALPN.into()];
+
+        let transport = quinn::TransportConfig::default();
+        client_endpoint_builder.default_client_config(quinn::ClientConfig {
+            crypto: Arc::new(tls_config),
+            transport: Arc::new(transport),
+        });
+    }
+
     let (client_endpoint, _) = client_endpoint_builder
         .bind(&"[::]:0".parse().unwrap())
         .unwrap();
@@ -73,4 +107,17 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     Ok(())
+}
+
+struct YesVerifier;
+impl rustls::ServerCertVerifier for YesVerifier {
+    fn verify_server_cert(
+        &self,
+        _roots: &rustls::RootCertStore,
+        _presented_certs: &[rustls::Certificate],
+        _dns_name: webpki::DNSNameRef,
+        _ocsp_response: &[u8],
+    ) -> std::result::Result<rustls::ServerCertVerified, rustls::TLSError> {
+        Ok(rustls::ServerCertVerified::assertion())
+    }
 }
