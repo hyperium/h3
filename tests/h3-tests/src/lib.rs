1f22583c91efefe4c3c2fac68bc070b251574302
Frank Spitulski <fspitulski@nvidia.com>
2021-11-15 14:45:47 -0800

    use quinn 0.8.0

--- tests/h3-tests/src/lib.rs
+++ tests/h3-tests/src/lib.rs
@@ -1,14 +1,16 @@
 use std::net::{Ipv6Addr, ToSocketAddrs};
+use std::sync::Arc;
 
 use bytes::Bytes;
 use futures::StreamExt;
+use rustls::{Certificate, PrivateKey};
+
 use h3::quic;
 use h3_quinn::{
-    quinn::{
-        Certificate, CertificateChain, ClientConfigBuilder, Endpoint, Incoming, PrivateKey,
-        ServerConfigBuilder,
-    },
     Connection,
+    quinn::{
+        Endpoint, Incoming,
+    },
 };
 
 pub fn init_tracing() {
@@ -22,35 +24,34 @@ pub fn init_tracing() {
 #[derive(Clone)]
 pub struct Pair {
     port: u16,
-    cert_chain: CertificateChain,
     cert: Certificate,
     key: PrivateKey,
 }
 
 impl Pair {
     pub fn new() -> Self {
-        let (cert_chain, cert, key) = build_certs();
+        let (cert, key) = build_certs();
         Self {
             port: 0,
-            cert_chain,
             cert,
             key,
         }
     }
 
     pub fn server(&mut self) -> Server {
-        let mut server_config = ServerConfigBuilder::default();
-        server_config.protocols(&[b"h3"]);
-        server_config
-            .certificate(self.cert_chain.clone(), self.key.clone())
+        let mut crypto = rustls::ServerConfig::builder()
+            .with_safe_default_cipher_suites()
+            .with_safe_default_kx_groups()
+            .with_protocol_versions(&[&rustls::version::TLS13])
+            .unwrap()
+            .with_no_client_auth()
+            .with_single_cert(vec![self.cert.clone()], self.key.clone())
             .unwrap();
+        crypto.max_early_data_size = u32::MAX;
+        crypto.alpn_protocols = vec![b"h3".to_vec()];
 
-        let mut server_endpoint_builder = Endpoint::builder();
-        server_endpoint_builder.listen(server_config.build());
-
-        let (endpoint, incoming) = server_endpoint_builder
-            .bind(&"[::]:0".parse().unwrap())
-            .unwrap();
+        let server_config = h3_quinn::quinn::ServerConfig::with_crypto(crypto.into());
+        let (endpoint, incoming) = h3_quinn::quinn::Endpoint::server(server_config, "[::]:0".parse().unwrap()).unwrap();
 
         self.port = endpoint.local_addr().unwrap().port();
 
@@ -64,21 +65,25 @@ impl Pair {
             .next()
             .unwrap();
 
-        let mut client_config = ClientConfigBuilder::default();
-        client_config.protocols(&[b"h3"]);
-        client_config
-            .add_certificate_authority(self.cert.clone())
-            .unwrap();
+        let mut root_cert_store = rustls::RootCertStore::empty();
+        root_cert_store.add(&self.cert).unwrap();
+        let mut crypto = rustls::ClientConfig::builder()
+            .with_safe_default_cipher_suites()
+            .with_safe_default_kx_groups()
+            .with_protocol_versions(&[&rustls::version::TLS13])
+            .unwrap()
+            .with_root_certificates(root_cert_store)
+            .with_no_client_auth();
+        crypto.enable_early_data = true;
+        crypto.alpn_protocols = vec![b"h3".to_vec()];
 
-        let mut client_endpoint_builder = Endpoint::builder();
-        client_endpoint_builder.default_client_config(client_config.build());
-        let (client_endpoint, _) = client_endpoint_builder
-            .bind(&"[::]:0".parse().unwrap())
-            .unwrap();
+        let mut client_config = h3_quinn::quinn::ClientConfig::new(Arc::new(crypto));
 
+        let mut client_endpoint = h3_quinn::quinn::Endpoint::client("[::]:0".parse().unwrap()).unwrap();
+        client_endpoint.set_default_client_config(client_config);
         Connection::new(
             client_endpoint
-                .connect(&addr, "localhost")
+                .connect(addr, "localhost")
                 .unwrap()
                 .await
                 .unwrap(),
@@ -96,9 +101,9 @@ impl Server {
     }
 }
 
-pub fn build_certs() -> (CertificateChain, Certificate, PrivateKey) {
+pub fn build_certs() -> (Certificate, PrivateKey) {
     let cert = rcgen::generate_simple_self_signed(vec!["localhost".into()]).unwrap();
-    let key = PrivateKey::from_der(&cert.serialize_private_key_der()).unwrap();
-    let cert = Certificate::from_der(&cert.serialize_der().unwrap()).unwrap();
-    (CertificateChain::from_certs(vec![cert.clone()]), cert, key)
+    let key = PrivateKey(cert.serialize_private_key_der());
+    let cert = Certificate(cert.serialize_der().unwrap());
+    (cert, key)
 }
