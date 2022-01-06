cbe4407c7cbac6782cd9245a7416ddbf36c63b80
Jean-Christophe BEGUE <jc.begue@pm.me>
2022-01-06 09:29:22 +0100

    Make the server example a file server
    And clean it up a bit

--- examples/server.rs
+++ examples/server.rs
@@ -1,55 +1,52 @@
-use std::path::PathBuf;
-use std::sync::Arc;
+use std::{net::SocketAddr, path::PathBuf, sync::Arc};
 
-use bytes::Bytes;
+use bytes::{Bytes, BytesMut};
 use futures::StreamExt;
-use h3::{quic::BidiStream, server::RequestStream};
+use http::{Request, StatusCode};
 use rustls::{Certificate, PrivateKey};
 use structopt::StructOpt;
-use tokio::fs::File;
-use tokio::io::AsyncReadExt;
-use tracing::{debug, error, info, trace, trace_span, warn};
+use tokio::{fs::File, io::AsyncReadExt};
+use tracing::{debug, error, info, trace_span, warn};
 
-static ALPN: &[u8] = b"h3";
+use h3::{quic::BidiStream, server::RequestStream};
 
-// Configs for two server modes
-// selfsigned mode will generate it's own local certificate
-// certs mode will require a path to 2-3 files(cert, key, ca)
 #[derive(StructOpt, Debug)]
 #[structopt(name = "server")]
 struct Opt {
-    #[structopt(subcommand)]
-    pub command: Command,
-}
+    #[structopt(
+        name = "dir",
+        short,
+        long,
+        help = "Root directory of the files to serve. \
+                If omited, server will respond OK."
+    )]
+    pub root: Option<PathBuf>,
 
-#[derive(StructOpt, Debug)]
-pub enum Command {
-    #[structopt(name = "selfsigned")]
-    SelfSigned(SelfSigned),
+    #[structopt(
+        short,
+        long,
+        default_value = "[::1]:4433",
+        help = "What adress:port to listen for new connections"
+    )]
+    pub listen: SocketAddr,
 
-    #[structopt(name = "certs")]
-    Certs(Certs),
-}
-
-#[derive(StructOpt, Debug)]
-pub struct SelfSigned {
-    #[structopt(long)]
-    pub debug: bool,
-
-    #[structopt(long, default_value = "4433")]
-    pub port: u16,
+    #[structopt(flatten)]
+    pub certs: Certs,
 }
 
 #[derive(StructOpt, Debug)]
 pub struct Certs {
-    #[structopt(long)]
-    pub cert: PathBuf,
+    #[structopt(
+        long,
+        short,
+        help = "Certificate for TLS. \
+                If present, `--key` is mandatory. \
+                If omitted, a selfsigned certificate will be generated."
+    )]
+    pub cert: Option<PathBuf>,
 
-    #[structopt(long)]
-    pub key: PathBuf,
-
-    #[structopt(long, default_value = "4433")]
-    pub port: u16,
+    #[structopt(long, short, help = "Private key for the certificate.")]
+    pub key: Option<PathBuf>,
 }
 
 #[tokio::main]
@@ -61,48 +58,28 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .init();
 
     let opt = Opt::from_args();
-    trace!("{:#?}", opt);
 
-    // quinn setup
-    let (cert, key, port) = match opt.command {
-        Command::SelfSigned(r) => {
-            let (cert, key) = build_certs();
-            (cert, key, r.port)
+    let root = if let Some(root) = opt.root {
+        if !root.is_dir() {
+            return Err(format!("{}: is not a readable directory", root.display()).into());
+        } else {
+            info!("serving {}", root.display());
+            Arc::new(Some(root))
         }
-        Command::Certs(c) => {
-            let mut cert_v = Vec::new();
-            let mut key_v = Vec::new();
-
-            let mut cert_f = File::open(c.cert).await?;
-            let mut key_f = File::open(c.key).await?;
-
-            cert_f.read_to_end(&mut cert_v).await?;
-            key_f.read_to_end(&mut key_v).await?;
-            (rustls::Certificate(cert_v), PrivateKey(key_v), c.port)
-        }
+    } else {
+        Arc::new(None)
     };
-    let mut crypto = rustls::ServerConfig::builder()
-        .with_safe_default_cipher_suites()
-        .with_safe_default_kx_groups()
-        .with_protocol_versions(&[&rustls::version::TLS13])
-        .unwrap()
-        .with_no_client_auth()
-        .with_single_cert(vec![cert], key)?;
-    crypto.max_early_data_size = u32::MAX;
-    crypto.alpn_protocols = vec![ALPN.into()];
+
+    let crypto = load_crypto(opt.certs).await?;
     let server_config = h3_quinn::quinn::ServerConfig::with_crypto(Arc::new(crypto));
+    let (endpoint, mut incoming) = h3_quinn::quinn::Endpoint::server(server_config, opt.listen)?;
 
-    let addr = format!("[::]:{:}", port).parse()?;
-    let (endpoint, mut incoming) = h3_quinn::quinn::Endpoint::server(server_config, addr)?;
-
-    info!(
-        "Listening on port {:?}",
-        endpoint.local_addr().unwrap().port()
-    );
+    info!("Listening on {}", opt.listen);
 
     while let Some(new_conn) = incoming.next().await {
         trace_span!("New connection being attempted");
 
+        let root = root.clone();
         tokio::spawn(async move {
             match new_conn.await {
                 Ok(conn) => {
@@ -113,31 +90,52 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                         .unwrap();
 
                     while let Some((req, stream)) = h3_conn.accept().await.unwrap() {
-                        debug!("connection requested: {:#?}", req);
+                        let root = root.clone();
+                        debug!("New request: {:#?}", req);
 
-                        tokio::spawn(handle_request(stream));
+                        tokio::spawn(async {
+                            if let Err(e) = handle_request(req, stream, root).await {
+                                error!("request failed: {}", e);
+                            }
+                        });
                     }
                 }
                 Err(err) => {
-                    warn!("connecting client failed with error: {:?}", err);
+                    warn!("accepting connection failed: {:?}", err);
                 }
             }
         });
     }
 
+    endpoint.wait_idle().await;
+
     Ok(())
 }
 
 async fn handle_request<T>(
+    req: Request<()>,
     mut stream: RequestStream<T, Bytes>,
-) -> Result<(), Box<dyn std::error::Error + Send>>
+    serve_root: Arc<Option<PathBuf>>,
+) -> Result<(), Box<dyn std::error::Error>>
 where
     T: BidiStream<Bytes>,
 {
-    let resp = http::Response::builder()
-        .status(http::StatusCode::NOT_FOUND)
-        .body(())
-        .unwrap();
+    let (status, to_serve) = match serve_root.as_deref() {
+        None => (StatusCode::OK, None),
+        Some(_) if req.uri().path().contains("..") => (StatusCode::NOT_FOUND, None),
+        Some(root) => {
+            let to_serve = root.join(req.uri().path().strip_prefix("/").unwrap_or(""));
+            match File::open(&to_serve).await {
+                Ok(file) => (StatusCode::OK, Some(file)),
+                Err(e) => {
+                    debug!("failed to open: \"{}\": {}", to_serve.to_string_lossy(), e);
+                    (StatusCode::NOT_FOUND, None)
+                }
+            }
+        }
+    };
+
+    let resp = http::Response::builder().status(status).body(()).unwrap();
 
     match stream.send_response(resp).await {
         Ok(_) => {
@@ -148,7 +146,49 @@ where
         }
     }
 
+    if let Some(mut file) = to_serve {
+        loop {
+            let mut buf = BytesMut::with_capacity(4096 * 10);
+            if file.read_buf(&mut buf).await? == 0 {
+                break;
+            }
+            stream.send_data(buf.freeze()).await?;
+        }
+    }
+
     Ok(stream.finish().await?)
+}
+
+static ALPN: &[u8] = b"h3";
+
+async fn load_crypto(opt: Certs) -> Result<rustls::ServerConfig, Box<dyn std::error::Error>> {
+    let (cert, key) = match (opt.cert, opt.key) {
+        (None, None) => build_certs(),
+        (Some(cert_path), Some(ref key_path)) => {
+            let mut cert_v = Vec::new();
+            let mut key_v = Vec::new();
+
+            let mut cert_f = File::open(cert_path).await?;
+            let mut key_f = File::open(key_path).await?;
+
+            cert_f.read_to_end(&mut cert_v).await?;
+            key_f.read_to_end(&mut key_v).await?;
+            (rustls::Certificate(cert_v), PrivateKey(key_v))
+        }
+        (_, _) => return Err("cert and key args are mutually dependant".into()),
+    };
+
+    let mut crypto = rustls::ServerConfig::builder()
+        .with_safe_default_cipher_suites()
+        .with_safe_default_kx_groups()
+        .with_protocol_versions(&[&rustls::version::TLS13])
+        .unwrap()
+        .with_no_client_auth()
+        .with_single_cert(vec![cert], key)?;
+    crypto.max_early_data_size = u32::MAX;
+    crypto.alpn_protocols = vec![ALPN.into()];
+
+    Ok(crypto)
 }
 
 pub fn build_certs() -> (Certificate, PrivateKey) {
