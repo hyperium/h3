6010c7744ee39084da90ab8597afcb124e924bfb
Julian Eager <eagr@tutanota.com>
2022-08-09 21:19:06 +0000

    Set up spec compliance checks (#114)
    Closes #109

---
+++ ci/compliance/report.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -e
+
+BLOB=${1:-master}
+
+./target/release/duvet \
+  report \
+  --spec-pattern 'ci/compliance/specs/**/*.toml' \
+  --source-pattern 'h3/**/*.rs' \
+  --workspace \
+  --exclude duvet \
+  --require-tests false \
+  --blob-link "https://github.com/hyperium/h3/blob/$BLOB" \
+  --issue-link 'https://github.com/hyperium/h3/issues' \
+  --no-cargo \
+  --html ci/compliance/report.html
+
+echo "compliance report available in 'ci/compliance/report.html'"
