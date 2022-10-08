ba8f7110acb8f04df3e0302b07c4a9a27f698516
Julian Eager <eagr@tutanota.com>
2022-10-08 19:55:06 +0800

    improve compliance ci action

--- ci/compliance/report.sh
+++ ci/compliance/report.sh
@@ -4,17 +4,16 @@ set -e
 
 BLOB=${1:-master}
 
-duvet \
-  report \
-  --spec-pattern 'specs/**/*.toml' \
-  --spec-pattern 'ci/compliance/specs/**/*.toml' \
-  --source-pattern 'h3/**/*.rs' \
-  --workspace \
-  --exclude duvet \
-  --require-tests false \
-  --blob-link "https://github.com/hyperium/h3/blob/$BLOB" \
-  --issue-link 'https://github.com/hyperium/h3/issues' \
-  --no-cargo \
-  --html ci/compliance/report.html
+duvet report \
+    --spec-pattern 'specs/**/*.toml' \
+    --spec-pattern 'ci/compliance/specs/**/*.toml' \
+    --source-pattern 'h3/**/*.rs' \
+    --workspace \
+    --exclude duvet \
+    --require-tests false \
+    --blob-link "https://github.com/hyperium/h3/blob/$BLOB" \
+    --issue-link 'https://github.com/hyperium/h3/issues' \
+    --no-cargo \
+    --html ci/compliance/report.html
 
 echo "compliance report available in 'ci/compliance/report.html'"
