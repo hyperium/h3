c4829609f699a03aac51ef6f70a96a8472f9250d
Dario Lencina <dario@securityunion.dev>
2023-03-31 22:43:48 -0400

    delete mkcert script because it does not work and it is confusing

--- examples/generate_certs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# use mkcert to generate certs for localhost
-mkcert -key-file localhost-key.pem -cert-file localhost.pem localhost 127.0.0.1 ::1
-
-# convert certs from pem to der
-openssl x509 -inform pem -outform der -in localhost.pem -out localhost.crt
-openssl rsa -inform pem -outform der -in localhost-key.pem -out localhost.key
