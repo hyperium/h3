d0bb808a119674d0e9d4151c988be72523599843
Dario Lencina <dario@securityunion.dev>
2023-04-01 19:29:19 -0400

    enable chrome debugging

--- examples/launch_chrome.sh
+++ examples/launch_chrome.sh
@@ -9,4 +9,6 @@ echo "Got cert key $SPKI"
 echo "Opening google chrome"
 
 sleep 2
-open -a "Google Chrome" --args --origin-to-force-quic-on=127.0.0.1:4433  --ignore-certificate-errors-spki-list=$SPKI
+open -a "Google Chrome" --args --origin-to-force-quic-on=127.0.0.1:4433 --ignore-certificate-errors-spki-list=$SPKI --enable-logging --v=1 
+
+## Logs are stored to ~/Library/Application Support/Google/Chrome/chrome_debug.log
